19ae1daea88d749db89f7425fa044db00094c4a1
youssef.m.guirguis@gmail.com <youssef.m.guirguis@gmail.com>
2021-01-05 16:56:28 -0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,27 @@
 // console.error(err);
 // })
 
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import microConfig from "./mikro-orm.config";
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+
+
+
+const main = async () =>{
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+//    const post = orm.em.create(Post , {title: "first post"}); // not actually poutting a post
+//    await orm.em.persistAndFlush(post); //we add to table. 
+//    await orm.em.nativeInsert(Post, {title: 'first post'})
+      const posts = await orm.em.find(Post, {});
+      console.log(posts);
+};
+
+main().catch((err) => {
+    console.error(err);
+});
+
+
+
+// console.log("hello world");
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM} from "@mikro-orm/core";
+import path from "path";
+
+export default {
+    migrations:{
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations, use path.join with dir__name to use the right path
+        pattern: /^[\w-]+\d+\.[tj]s$/ //add [tj] to accept javascript too
+    },
+    entities: [ Post ], //for our database tables
+    dbName: 'web',
+    user: 'postgres',
+    password: 'password',
+    debug: !__prod__,
+    type: 'postgresql'
+}as Parameters<typeof MikroORM.init>[0]; //type issue
+
