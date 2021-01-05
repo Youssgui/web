6953a353c47ee0a6cc3c0f9a2ff359907b1a203b
youssef.m.guirguis@gmail.com <youssef.m.guirguis@gmail.com>
2021-01-05 18:32:05 -0500

    express setup

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,28 @@ import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
-
+import express from 'express';
 
 
 const main = async () =>{
+
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
+
 //    const post = orm.em.create(Post , {title: "first post"}); // not actually poutting a post
 //    await orm.em.persistAndFlush(post); //we add to table. 
 //    await orm.em.nativeInsert(Post, {title: 'first post'})
-      const posts = await orm.em.find(Post, {});
-      console.log(posts);
+    //   const posts = await orm.em.find(Post, {});
+    //   console.log(posts);
+
+      const app = express(); //setting up express app
+      app.get('/', (_, res) => { //makinga get endpoint, '/' is homepage
+            res.send("ea");
+      })
+      app.listen(4000, () =>{ //setting up app on localhost, doesnt actually connect the server. express builds a rest API
+        console.log('server has been setup on localhost:4000')
+      })
+
 };
 
 main().catch((err) => {
