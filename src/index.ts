0fdd18e49983a7748dfc17acff9c26d79eff61d2
youssef.m.guirguis@gmail.com <youssef.m.guirguis@gmail.com>
2021-01-07 09:16:16 -0500

    redis setup

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ import {buildSchema} from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { MyContext } from "./types";
+
 
 
 const main = async () =>{
@@ -31,13 +36,35 @@ const main = async () =>{
     //   console.log(posts);
 
       const app = express(); //setting up express app
+
+      const RedisStore = connectRedis(session) // middleware for accessing redis, order matter here. should be between app and applying middleware. copied from the connectredis github page
+      const redisClient = redis.createClient() //redis client. oreder matters because we will be using session inside apollo middleware
+      
+      app.use(  //redis  stuff
+        session({
+          name : 'bob',
+          store: new RedisStore({ 
+            client: redisClient,
+            disableTouch: true // here, this option refers to how long we will save the cookie. disabletouch means we will save the cookie indefinitely. touch means that if the user interacts or clicks a button, it extends the cookie session life
+          }),
+          cookie : { //cookie stuff
+            maxAge : 1000 * 60 * 60 * 24 * 365 * 10, //time in millisseconds ie 10 years
+            httpOnly : true, // this is for security, in frontend js, it cannot access the cookie
+            sameSite: 'lax', //crsf attack protection. cookies are a thing which keeps some information about the user on our side, used in say "display every x number of visits" 
+            secure : __prod__ //means cookie only works in https 
+          },
+          secret: 'fjaulcjmuacmoialjfhgf', //this is how its gonna assign the cookie, will make into environment variable
+          resave: false,
+        })
+      )
+
       const apolloServer = new ApolloServer({ //here we create a graphql endpoint
         schema: await  buildSchema({// here we pass  our options 
-          resolvers: [HelloResolver, PostResolver, UserResolver],  //resolvers are repsonsible for populating the data for a single field in schema, place here all resolvers
+          resolvers: [HelloResolver, PostResolver, UserResolver],  //graphql resolvers are repsonsible for populating the data for a single field in schema, place here all resolvers
           //so the resolver is a simple schema, and our apollo server uses that schema
           validate: false, //something annoying disabled 
         }), //we use await wwhen our function returns a promise
-        context : () => ({ em: orm.em }) //special object that access all these resolvers. we need the em object from orm 
+        context : ({req,res}): MyContext => ({ em: orm.em, req,res }), //special object that access all these resolvers. usually this is the db instance, so we need the em object from orm 
       });
 
       apolloServer.applyMiddleware({app}); //this commands creates a graphql endpoint for us on express
